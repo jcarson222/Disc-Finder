94bc5de01916c633589f28f68b7a5f29ccc74ba4
Jesse Carson <jmcarson222@gmail.com>
2023-08-15 16:09:47 -0500

    BrowserRouter/ Pages/ Nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,55 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
+// import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { UserProfile, Search, Home, HomeLayout } from "./pages";
+
+// const queryClient = new QueryClient({
+//   defaultOptions: {
+//     queries: {
+//       staleTime: 1000 * 60 * 5,
+//     },
+//   },
+// });
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomeLayout />,
+    // errorElement: <Error />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: "search",
+        element: <Search />,
+      },
+      {
+        path: "profile",
+        element: <UserProfile />,
+      },
+      // {
+      //   path: "register",
+      //   element: <Register />,
+      //   action: registerAction,
+      // },
+      // {
+      //   path: "login",
+      //   element: <Login />,
+      //   action: loginAction,
+      // },
+    ],
+  },
+]);
 
 const App = () => {
-  return <h1>Hello World</h1>;
+  return <RouterProvider router={router} />;
 };
 
 export default App;
+
+// <QueryClientProvider client={queryClient}>
+//   <RouterProvider router={router} />
+//   <ReactQueryDevtools initialIsOpen={false} />
+// </QueryClientProvider>
---
+++ src/pages/Search.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Search = () => {
+  return <h2>Search Page</h2>;
+};
+
+export default Search;
