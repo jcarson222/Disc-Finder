7cfaba9d7025cbd14541227d2900fd6051bf35f4
Jesse Carson <jmcarson222@gmail.com>
2023-08-17 09:05:45 -0500

    featured discs

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import Hero from "../components/Hero";
+import { Hero, FeaturedDiscs } from "../components";
 
 const Home = () => {
   return (
     <main>
       <Hero />
+      <FeaturedDiscs />
     </main>
   );
 };
--- utils/constants.js
+++ utils/constants.js
@@ -19,3 +19,39 @@ export const links = [
 export const tempUser = {
   name: "jesse",
 };
+
+export const tempDiscs = [
+  {
+    name: "Firebird",
+    id: 1,
+    speed: 9,
+    glide: 3,
+    turn: 0,
+    fade: 4,
+    links: "https://infinitediscs.com/Innova-Firebird",
+    manufacturer: "Innova",
+    img: "https://infinitediscs.com/Inf_Uploads/DiscProducts/Firebird.Webp",
+  },
+  {
+    name: "Wraith",
+    id: 2,
+    speed: 11,
+    glide: 5,
+    turn: -1,
+    fade: 3,
+    links: "https://infinitediscs.com/Innova-Wraith",
+    manufacturer: "Innova",
+    img: "https://infinitediscs.com/Inf_Uploads/DiscProducts/Wraith.Webp",
+  },
+  {
+    name: "Element",
+    id: 3,
+    speed: 5,
+    glide: 5,
+    turn: -2,
+    fade: 1,
+    links: "https://infinitediscs.com/Gateway-Element",
+    manufacturer: "Gateway",
+    img: "https://infinitediscs.com/Inf_Uploads/DiscProducts/Element.Webp",
+  },
+];
